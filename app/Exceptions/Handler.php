5b2317169e29897f156ff142c2a2afbb61adc738
Veselin Romić <omegavesko@gmail.com>
2018-04-28 16:07:58 +0200

    Remove legacy frontend

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,11 +44,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if ($exception instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException)
-        {
-            return response()->view('main');
-        }
-
         return parent::render($request, $exception);
     }
 
--- public/node_modules/angular-slugify/index.js
+++
@@ -1,3 +0,0 @@
-var angularSlugify = require('angular-slugify');
-
-module.exports = angularSlugify;
