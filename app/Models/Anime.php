d5610bfb262e4a439cc5b91ba3c3767c83b88c9b
Veselin Romić <omegavesko@gmail.com>
2018-04-29 15:28:22 +0200

    Restricted visible fields on Anime model

--- app/Models/Anime.php
+++ app/Models/Anime.php
@@ -11,6 +11,7 @@ use Illuminate\Database\Eloquent\Model;
 class Anime extends Model
 {
     protected $table = 'anime';
+    protected $visible = ['id', 'title', 'members', 'rating', 'archived'];
 
     public function snapshots()
     {
