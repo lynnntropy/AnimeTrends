c6d54d4b9acc51f5e18943c9c8ca0c91c726a321
Veselin Romić <omegavesko@gmail.com>
2018-05-01 02:24:49 +0200

    Move environment variables to .env

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -47,10 +47,14 @@ module.exports = {
     }
   },
 
-  env: {
-    apiBaseUrl: process.env.NODE_ENV !== 'production' ? 'http://localhost:8000/api' : '/api',
-    backendBaseUrl: process.env.NODE_ENV !== 'production' ? 'http://localhost:8000/' : '/',
-  },
+  // env: {
+  //   apiBaseUrl: process.env.NODE_ENV !== 'production' ? 'http://localhost:8000/api' : '/api',
+  //   backendBaseUrl: process.env.NODE_ENV !== 'production' ? 'http://localhost:8000/' : '/',
+  // },
+
+  modules: [
+    '@nuxtjs/dotenv'
+  ],
 
   plugins: [
     '~/plugins/axios-defaults',
