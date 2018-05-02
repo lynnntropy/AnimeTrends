5abfced28c2afbbfde0cec680576030e38b91ffe
Veselin Romić <omegavesko@gmail.com>
2018-05-02 16:57:18 +0200

    Add favicon(s)

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -9,10 +9,19 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'See MyAnimeList score charts of various anime on AnimeTrends!' }
+      { hid: 'description', name: 'description', content: 'See MyAnimeList score charts of various anime on AnimeTrends!' },
+      { property: 'og:site_name', content: 'AnimeTrends' },
+
+      { name: "msapplication-TileColor", content: "#ffc40d" },
+      { name: "theme-color", content: "#ffc40d" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "apple-touch-icon", sizes: "120x120", href: "/apple-touch-icon.png" },
+      { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+      { rel: "manifest", href: "/site.webmanifest" },
+      { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" }
     ]
   },
 
