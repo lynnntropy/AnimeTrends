10459fbb1818afc1422b2eff29cd27e3fa4055ef
Veselin Romić <omegavesko@gmail.com>
2018-05-03 21:57:18 +0200

    Add episode model and table

---
+++ backend/database/migrations/2018_05_03_195200_create_episodes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEpisodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('anime_id');
+            $table->integer('episode_number');
+            $table->string('title');
+            $table->string('title_romaji');
+            $table->string('title_japanese');
+            $table->date('aired_date');
+            $table->timestamps();
+
+            $table->foreign('anime_id')->references('id')->on('anime');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('episodes');
+    }
+}
